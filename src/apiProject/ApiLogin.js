1ee4101105601f5713ac00541268007cc924fe9c
Apple <tranlenhathoang357@gmail.com>
2025-01-18 13:50:28 +0700

    edit

--- src/apiProject/ApiLogin.js
+++ src/apiProject/ApiLogin.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export async function checkLogin(username, password){
     try{
         const response = await axios.get("http://localhost:3001/users");
-        const users = response.data.users;
+        const users = response.data;
         const user = users.find(user => user.name === username && user.password === password);
         return user || null;
     }catch(e){
