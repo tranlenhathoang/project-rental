18111e279bd061eed47f382b28b79222d9cbbf7e
ss3_code <dinhhang440@gmail.com>
2025-02-15 18:26:52 +0700

    sửa phần ảnh của avatar

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -72,6 +72,7 @@ const Header = () => {
 											width={40}
 											height={40}
 											className="border border-primary"
+											style={{ objectFit: "cover" }}
 										/>
 									}
 									align="end"
